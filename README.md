0dae0be124268932b08e89156e14bc91999897dc
Owais Master <owais.master@uconn.edu>
2019-11-12 00:31:32 -0500

    Fully working Bamazon

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Bamazon
+
+Once ```node bamazonCustomer.js``` is ran it will show you all currently available products and you will be prompted to pick a product by its ID. Once the ID is entered, you will be prompted for the quantity you would like to purchase. If quantity is available, your order will be placed and you will be asked if you want to continue.
+
+![Successful Transaction](/images/bamazonsuccessNocontinue.gif)
+
+
+If quantity is unavailable then you will be informed and your order will be canceled. Then the user is prompted again whether they want to continue. If they choose to continue the products will be displayed again and the prompt restarts. If they want to quit shopping at any moment, entering 'x' will shutdown the program.
+
+![Insufficent Quantity](/images/bamazonInsufficent.gif)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,9 +2,8 @@ const mysql = require("promise-mysql");
 const inquirer = require("inquirer");
 const config = require('./config.js');
 const chalk = require('chalk');
-require('console.table');
-let response;
-let inquirer2;
+require('dotenv').config();
+
 
 async function startBamazon() {
     const connection = await mysql.createConnection(config);
@@ -12,49 +11,82 @@ async function startBamazon() {
         console.log(chalk.bold.underline.bgBlue.yellow('\r\nProducts Currently Available: \r\n'));
         for (var i = 0; i < response.length; i++) {
             let product = response[i];
-            if (product.stock_quantity > 0) {
-                let table = chalk.blue(`Id: ${chalk.red(product.item_id)} 
+            if (parseInt(product.stock_quantity) > 0) {
+                console.log(chalk.blue(`
+                Id: ${chalk.red(product.item_id)} 
                 Name: ${chalk.yellow(product.product_name)}
                 Price: ${chalk.green('$' + product.price)} 
-                Department: ${chalk.magenta(product.department_name)}\r\n`)
-                console.table(table);
+                Department: ${chalk.magenta(product.department_name)}
+                Quantity: ${chalk.yellow(product.stock_quantity)}\r\n`));
             }
         }
-        inquirer2 = inquirer.prompt([
+        inquirer.prompt([
             {
                 type: 'input',
                 name: 'id',
-                message: "What is the ID of the item you would like to purchase?",
-
+                message: `What is the ID of the item you would like to purchase? ${chalk.red(['Enter "x" to exit anytime'])}`,
+                validate: function (val) {
+                    if (val === "x") {
+                        process.exit(0);
+                    } else if (!isNaN(val)) {
+                        return !isNaN(val);
+                    };
+                }
             },
             {
                 type: 'input',
                 name: 'quantity',
-                message: 'How many of this product would you like to purchase?'
+                message: 'How many of this product would you like to purchase?',
+                validate: function (val) {
+                    return val > 0 || val.toLowerCase() === "x";
+                }
             }
         ]).then(input => {
+            if (input.id === 'x' || input.quantity === 'x') {
+                process.exit(0);
+            }
             console.log(chalk.yellow('Processing Transaction...'));
             for (var i = 0; i < response.length; i++) {
-                console.log("THIS WORKS");
                 let product = response[i];
                 let quantity = product.stock_quantity;
                 if (input.id == product.item_id) {
                     if (quantity >= input.quantity) {
                         newQuantity = quantity - input.quantity;
                         connection.query(`UPDATE products SET stock_quantity=${product.stock_quantity - input.quantity} WHERE item_id=${product.item_id}`).then((response) => {
-                            console.log("THIS DOESNT WORK");
                             connection.end();
                         })
-                        console.log(chalk.blue("Your order has been placed for a total of ") + chalk.green('$' + input.quantity * product.price));
-
+                        console.log(chalk.blue.bgBlack("Your order has been placed for a total of ") + chalk.green('$' + input.quantity * product.price));
+                        askContinue();
                     } else {
-                        console.log('Insufficient quantity!');
-                        connection.end();
+                        console.log(chalk.red.bold('Insufficient quantity!'));
+                        askContinue();
+
                     }
-                } break;
+                    break;
+                };
             }
         });
     })
 };
+
+
+function askContinue() {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'exit',
+            message: "Would you like to continue? [y/n]",
+        }
+    ]).then(input => {
+        var choice = input.exit
+        if (choice === false) {
+            console.log(chalk.bold.underline.bgCyan.white("Keeping checking back for new products!"));
+            process.exit(0);
+        } else if (choice === true) {
+            startBamazon();
+        }
+    })
+};
+
 
 startBamazon();
